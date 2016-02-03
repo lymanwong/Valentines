304fca5368dd486cb48f956267ddd00a1ecdaca7
lyman wong <lymanwong@gmail.com>
2016-02-02 17:09:16 -0800

    add page refresh

--- public/js/application.js
+++ public/js/application.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
   $('.next_button').click(function(){
     location.reload();
   });
+  $('.next_button').mouseover(function(){
+    $(this).addClass('hovering');
+  }).mouseleave(function(){
+    $(this).removeClass('hovering');
+  });
+  $('.submit_button').mouseover(function(){
+    $(this).addClass('hovering');
+  }).mouseleave(function(){
+    $(this).removeClass('hovering');
+  });
+
 });
+
+setTimeout(function(){
+   window.location.reload(1);
+}, 5000);
