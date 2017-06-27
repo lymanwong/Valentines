1a8a68c7b3155e5a51fc6e0768147a0c645f2f28
lyman wong <lymanwong@gmail.com>
2017-06-27 16:24:15 -0700

    added background video

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-  $('.wrapper_bottom').hide();
+  scaleVideoContainer();
+
+  initBannerVideoSize('.video-container .poster img');
+  initBannerVideoSize('.video-container .filter');
+  initBannerVideoSize('.video-container video');
+
+  $(window).on('resize', function() {
+      scaleVideoContainer();
+      scaleBannerVideoSize('.video-container .poster img');
+      scaleBannerVideoSize('.video-container .filter');
+      scaleBannerVideoSize('.video-container video');
+  });
+  $('#form').hide();
   $('.add_button').prop('title','Click me to send Marss some love <3');
   $('.add_button').click(function(){
-    $('.wrapper_bottom').fadeToggle("slow");
+    $('#form').fadeToggle("slow");
   });
   $('#fader').fadeIn(1000);
   $('.next_button').click(function(){
@@ -35,3 +47,49 @@ $(document).ready(function() {
     window.alert("Thanks for sharing your love via AJAX!");
   });
 });
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    // console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+  }
